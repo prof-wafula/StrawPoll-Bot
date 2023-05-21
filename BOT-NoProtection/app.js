f6707186c26c782d49ae7b8995e7a5c03c56b2fd
prof-wafula <134214707+prof-wafula@users.noreply.github.com>
2023-05-21 19:30:38 +0300

    Update app.js

--- BOT-NoProtection/app.js
+++ BOT-NoProtection/app.js
@@ -2,4 +2,4 @@ const botPoll = require('strawpoll-bots').default;
 console.log("Iniciando bot :)");
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://strawpoll.com/polls/e6Z28v24XnN', 2, false, null, 2000);
+botPoll('https://strawpoll.com/polls/e6Z28v24XnN', 2, false, null, 200);
