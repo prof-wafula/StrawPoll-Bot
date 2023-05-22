0020da456e5597740692c63afa141a0926a12b1d
prof-wafula <134214707+prof-wafula@users.noreply.github.com>
2023-05-22 05:09:24 +0000

    Pending changes exported from your codespace

--- BOT-NoProtection/app.js
+++ BOT-NoProtection/app.js
@@ -2,4 +2,4 @@ const botPoll = require('strawpoll-bots').default;
 console.log("Iniciando bot :)");
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311241', 1, false, null, 2000);
+botPoll('https://www.strawpoll.com/polls/e6Z28v24XnN
